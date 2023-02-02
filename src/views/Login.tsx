1531006210f2a310d6fe15df5b0f4aaabc5e0e9d
Gure-san <azrarafani@gmail.com>
2023-02-02 14:57:34 +0700

    feat: membuat halaman login

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React from 'react';
 
 export default function Login() {
   return (
-    <section>
-      <h1>SPS</h1>
-      <p>Sistem Presensi Siswa</p>
-    </section>
-  )
+    <div className='w-[100vw] h-[100vh] flex items-center justify-center'>
+      <div className='  -translate-y-6'>
+        {/* Header */}
+        <div className='flex flex-col items-center'>
+          <h1 className='font-bold text-2xl'>SPS</h1>
+          <p>Sistem Presensi Siswa</p>
+        </div>
+
+        {/* Body (Form) */}
+        <form className='flex flex-col items-center my-6'>
+          <h2 className='font-bold mb-2 text-xl'>Login</h2>
+          <div className='flex flex-col w-full'>
+            <input
+              type='text'
+              className='px-4 py-2 mt-2 border border-dark-100 bg-white focus:border-black rounded-md text-sm '
+              placeholder='Nama Lengkap'
+            />
+            <input
+              type='text'
+              className='px-4 py-2 mt-2 border border-dark-100 bg-white focus:border-black rounded-md text-sm '
+              placeholder='Password'
+            />
+          </div>
+          <button className='bg-black text-white text-sm w-full py-2 mt-4 rounded-md'>
+            Continue
+          </button>
+        </form>
+
+        {/* Footer */}
+        <div className='flex flex-col items-center'>
+          <p className='text-sm font-semibold cursor-pointer'>
+            Tidak bisa login?
+          </p>
+          <p className='text-sm underline cursor-pointer'>
+            Hubungi Admin untuk mengatasi login kamu
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -26,6 +26,7 @@ module.exports = {
     },
 
     colors: {
+      ...colors,
       black: colors.black,
       white: colors.white
     }
